0adcf9351079bb8f05602d02e5304a5183cc8974
Behzad Sharafi <behzadsharafi@gmail.com>
2023-11-15 22:45:56 +1100

    updates the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <img src="src\assets\Header.jpg" alt="Header Image" />
+      <img
+        src="https://www.inc-aus.com/wp-content/uploads/sites/2/2023/02/getty_655998316_2000149920009280219_363765.jpg?w=733"
+        alt="Header Image"
+      />
       <h1 className={styles.header__title}>
         We help you find amazing books on any topic!
       </h1>
