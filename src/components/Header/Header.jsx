f414bff95fef6acc3fb3a2d45f14bea6cb4533b3
Behzad Sharafi <behzadsharafi@gmail.com>
2023-11-15 22:54:10 +1100

    updates header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import styles from "./Header.module.scss";
+import HeaderImage from "../../assets/Header.jpg";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <img src="../../src/assets/React.png" alt="Header Image" />
+      <img src={HeaderImage} alt="Header Image" />
       <h1 className={styles.header__title}>
-        We help you find amazing books on any topic!!
+        We help you find amazing books on any topic!
       </h1>
     </div>
   );
