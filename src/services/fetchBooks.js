eb7465c391964227b7020b9332b6d5e693f8f607
Behzad Sharafi <behzadsharafi@gmail.com>
2024-01-14 16:34:32 +1100

    adds an environment variable

--- src/services/fetchBooks.js
+++ src/services/fetchBooks.js
@@ -17,14 +17,24 @@ const cleanedData = (data) => {
 };
 
 export const fetchBooks = async (searchTerm) => {
+  const apiKey = process.env.API_KEY;
   const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`,
+    `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&maxResults=20&key=${apiKey}`,
     {
       headers: {
         Accept: "application/json",
       },
     }
   );
+
+  // const response = await fetch(
+  //   `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`,
+  //   {
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   }
+  // );
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
